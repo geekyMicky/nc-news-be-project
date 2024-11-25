aeb1c3fd728042d12910b7aae454e073ad456f32
geekyMicky <yangmiqixu@gmail.com>
2024-11-25 16:22:41 +0000

    task 2 done

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { getAllNews } = require("./api.controller");
+const { getAllNews, getAllTopics } = require("./api.controller");
 
 const app = express();
 
 app.get("/api", getAllNews);
+
+app.get("/api/topics", getAllTopics);
+
+app.use((req, res, next) => {
+  res.status(404).send({ error: "Not Found" });
+});
 
 module.exports = app;
