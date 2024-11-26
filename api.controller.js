990fa6459a08a7bb95a86280c72f483172fd0897
geekyMicky <yangmiqixu@gmail.com>
2024-11-26 10:19:24 +0000

    task 4 done

--- api.controller.js
+++ api.controller.js
@@ -1,5 +1,5 @@
 const endpointsJson = require("./endpoints.json");
-const { fetchAllTopics } = require("./app.model");
+const { fetchAllTopics, fetchArticleById } = require("./app.model");
 
 exports.getAllNews = (req, res) => {
   res.status(200).send({ endpoints: endpointsJson });
@@ -9,6 +9,18 @@ exports.getAllTopics = (req, res, next) => {
   fetchAllTopics()
     .then((topics) => {
       res.status(200).send({ topics });
+    })
+    .catch(next);
+};
+
+exports.getArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  if (isNaN(article_id)) {
+    return next({ status: 400, msg: "Bad Request" });
+  }
+  fetchArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
     })
     .catch(next);
 };
